51c4f6c04ecc065a2cd64f9bf029e6441f373bb7
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-08-17 12:25:05 +0300

    Initial commit

---
+++ 1. PyTorch CNN Tutorial MNIST/pytorchCNN.py
@@ -0,0 +1,27 @@
+# Import PyTorch
+import torch
+
+# We use torchvision to get our dataset and useful image transformations
+import torchvision
+import torchvision.transforms as transforms
+
+# Import PyTorch's optimization libary and nn
+# nn is used as the basic building block for our Network graphs
+import torch.optim as optim
+import torch.nn as nn
+
+# Are we using our GPU?
+
+print("GPU available: {}".format(torch.cuda.is_available()))
+
+
+#If GPU is available set device = 'cuda' if not set device = 'cpu'
+
+if torch.cuda.is_available():
+  device = 'cuda'
+else:
+  device = 'cpu'
+
+  # Transform to a PyTorch tensors and the normalize our valeus between -1 and +1
+  transform = transforms.Compose([transforms.ToTensor(),
+                                  transforms.Normalize((0.5,), (0.5,))])
